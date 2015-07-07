16e8e0fd211f7195f06d63512d2f6224d2b8fc17
MaoMao <scodemao@gmail.com>
2015-07-07 15:53:34 +0800

    handled = (boolean) m.invoke(view, SwipeLayout.this, p, vId);
    handled = (boolean) m.invoke(view, SwipeLayout.this, p, vId);

---
+++ gen/R.java.d
@@ -0,0 +1,3 @@
+/Users/maoweiwei/Projects/develop-chanyouji/local_libs/SwipeLayout/gen/com/daimajia/swipe/R.java \
+ : /Users/maoweiwei/Projects/develop-chanyouji/local_libs/SwipeLayout/res/values/attrs.xml \
+/Users/maoweiwei/Projects/develop-chanyouji/local_libs/SwipeLayout/bin/AndroidManifest.xml \
--- gen/com/daimajia/swipe/BuildConfig.java
+++ gen/com/daimajia/swipe/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.daimajia.swipe;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
